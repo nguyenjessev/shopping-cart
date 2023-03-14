cdcf51e7565d8832c7bb50e307f96613e92a2f00
Jesse Nguyen <nguyenjessev@gmail.com>
2023-03-14 19:51:18 -0400

    Style shop page and tiles

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,1 +1,78 @@
-<h1>Hello, shop!</h1>
+<script>
+  import ProductTile from './ProductTile.svelte';
+
+  const products = [
+    {
+      name: 'Model One',
+      price: 9999.99,
+      image: 'https://via.placeholder.com/400?text=Model+One',
+      id: Symbol('Model One'),
+    },
+    {
+      name: 'Model Two',
+      price: 5999.99,
+      image: 'https://via.placeholder.com/400?text=Model+Two',
+      id: Symbol('Model Two'),
+    },
+    {
+      name: 'Model Three',
+      price: 7999.99,
+      image: 'https://via.placeholder.com/400?text=Model+Three',
+      id: Symbol('Model Three'),
+    },
+    {
+      name: 'Model Four',
+      price: 12999.99,
+      image: 'https://via.placeholder.com/400?text=Model+Four',
+      id: Symbol('Model Four'),
+    },
+    {
+      name: 'Model Five',
+      price: 1999.99,
+      image: 'https://via.placeholder.com/400?text=Model+Five',
+      id: Symbol('Model Five'),
+    },
+    {
+      name: 'Model Six',
+      price: 999.99,
+      image: 'https://via.placeholder.com/400?text=Model+Six',
+      id: Symbol('Model Six'),
+    },
+    {
+      name: 'Model Seven',
+      price: 2499.99,
+      image: 'https://via.placeholder.com/400?text=Model+Seven',
+      id: Symbol('Model Seven'),
+    },
+  ];
+
+  const addToCart = (product) => {
+    alert(product);
+  };
+</script>
+
+<div class="container">
+  <div class="product-grid">
+    {#each products as product}
+      <ProductTile {...product} on:addToCart={() => addToCart(product)} />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    background: radial-gradient(hsl(0, 0%, 70%) 10%, hsl(0, 0%, 88%) 10%);
+    background-size: 2vmin 2vmin;
+    overflow: scroll;
+    padding: 8vmin 4vmin;
+  }
+
+  .product-grid {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    justify-items: center;
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+</style>
---
+++ src/routes/shop/ProductTile.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let name;
+  export let price;
+  export let image;
+
+  const dispatch = createEventDispatcher();
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const addToCart = () => {
+    dispatch('addToCart');
+  };
+</script>
+
+<div class="tile">
+  <img src={image} alt="Product" class="product-img" />
+  <div class="card-bottom">
+    <div class="product-info">
+      <h3 class="product-name">{name}</h3>
+      <div class="product-price">{formatter.format(price)}</div>
+    </div>
+    <button class="add-to-bag-btn" on:click={addToCart}>Add to Bag</button>
+  </div>
+</div>
+
+<style>
+  .tile {
+    background: hsl(40, 50%, 95%);
+    border: 1px solid hsl(207, 95%, 8%);
+    border-radius: 10px;
+    box-shadow: -8px 8px 20px -8px hsl(207, 95%, 8%);
+    color: black;
+    max-width: 100%;
+    overflow: hidden;
+  }
+
+  .product-img {
+    max-width: 100%;
+  }
+
+  .card-bottom {
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem;
+  }
+
+  .product-name {
+    margin: 0;
+  }
+
+  .add-to-bag-btn {
+    background-color: hsl(113, 39%, 46%);
+    border: none;
+    border-radius: 10px;
+    font-weight: bold;
+    padding: 1rem;
+  }
+
+  .add-to-bag-btn:hover {
+    background-color: hsl(113, 49%, 46%);
+    cursor: pointer;
+  }
+</style>
